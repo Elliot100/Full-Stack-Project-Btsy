518304e8033a7245f3e024ca249898caa93a19d0
Elly <ellychau10@yahoo.com>
2022-01-22 17:41:50 -0800

    Create first product seed

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_23_000352) do
+ActiveRecord::Schema.define(version: 2022_01_23_012647) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "products", force: :cascade do |t|
+    t.string "title"
+    t.string "image"
+    t.decimal "price", precision: 8, scale: 2
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username", null: false
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,10 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 User.destroy_all
+Product.destroy_all
 
 user1 = User.create(username: Faker::Name.name, email: Faker::Internet.email, password: 'password')
 user2 = User.create(username: Faker::Name.name, email: Faker::Internet.email, password: 'password')
 user3 = User.create(username: Faker::Name.name, email: Faker::Internet.email, password: 'password')
+
+product1 = Product.create(title: "Minimalist Leather Wallet", image: "https://cdn.shopify.com/s/files/1/0883/8928/products/2-Vegetble-tanned-leather-minimalist-wallet-origami-foldable-light-brown.jpg?v=1609759752", price: 32.00, description: "Minimalist Leather Wallet, Pop Up Credit Card Wallet,Unisex Wallet")
