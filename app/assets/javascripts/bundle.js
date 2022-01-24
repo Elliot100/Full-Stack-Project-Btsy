197b0e18361516b32792b33289b7a528a4dba0c0
Elly <ellychau10@yahoo.com>
2022-01-24 10:29:50 -0800

    Home page filler product image

--- app/assets/javascripts/bundle.js
+++ app/assets/javascripts/bundle.js
@@ -269,19 +269,45 @@ var _react2 = _interopRequireDefault(_react);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
+// eslint-disable-next-line react/display-name
 exports.default = function () {
   return _react2.default.createElement(
     "div",
-    { className: "home" },
+    null,
     _react2.default.createElement(
       "div",
-      { className: "hero-img-frame" },
-      _react2.default.createElement("img", { className: "hero-img", src: "https://variety.com/wp-content/uploads/2017/02/angry-birds-blues-rovio.jpg" })
+      { className: "home" },
+      _react2.default.createElement(
+        "div",
+        { className: "hero-img-frame" },
+        _react2.default.createElement("img", { className: "hero-img", src: "https://variety.com/wp-content/uploads/2017/02/angry-birds-blues-rovio.jpg" })
+      ),
+      _react2.default.createElement(
+        "h1",
+        null,
+        "Find extra-special ways to express your love."
+      )
     ),
     _react2.default.createElement(
-      "h1",
-      null,
-      "Find extra-special ways to express your love."
+      "div",
+      { className: "product-img-frame" },
+      _react2.default.createElement("img", {
+        className: "product-img",
+        src: "https://media.istockphoto.com/photos/leather-minimalist-wallet-picture-id916776140?k=20&m=916776140&s=170667a&w=0&h=_GqFjqKp8_LP5_FFeMapbVFzMw7UMxEyiWNhLQYMasE="
+      }),
+      _react2.default.createElement("img", {
+        className: "product-img",
+        src: "https://homesourcetx-wpengine.netdna-ssl.com/wp-content/uploads/2021/02/Industrial-and-wooden-desk.jpg"
+      }),
+      _react2.default.createElement("img", {
+        className: "product-img",
+        src: "https://media.istockphoto.com/photos/yellow-artistic-lamp-with-rock-and-wooden-basement-on-the-table-picture-id1127365483?k=20&m=1127365483&s=170667a&w=0&h=mMKNACPJma1MCZPKJJsIZFzUMkVjxSxihn65Al-Zjq0="
+      }),
+      _react2.default.createElement("img", {
+        className: "product-img",
+        src: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSQ6eg1NED1_i7sG-GRzpTxaht2SOIGRG7XSA&usqp=CAU"
+      }),
+      _react2.default.createElement("img", { className: "product-img", src: "https://www.kids-world.com/images/LO886.jpg" })
     )
   );
 };
--- frontend/components/home/home.jsx
+++ frontend/components/home/home.jsx
@@ -1,10 +1,32 @@
 import React from "react";
 
+// eslint-disable-next-line react/display-name
 export default () => (
-  <div className="home">
-    <div className="hero-img-frame">
-      <img className="hero-img" src="https://variety.com/wp-content/uploads/2017/02/angry-birds-blues-rovio.jpg" />
+  <div>
+    <div className="home">
+      <div className="hero-img-frame">
+        <img className="hero-img" src="https://variety.com/wp-content/uploads/2017/02/angry-birds-blues-rovio.jpg" />
+      </div>
+      <h1>Find extra-special ways to express your love.</h1>
     </div>
-    <h1>Find extra-special ways to express your love.</h1>
+    <div className="product-img-frame">
+      <img
+        className="product-img"
+        src="https://media.istockphoto.com/photos/leather-minimalist-wallet-picture-id916776140?k=20&m=916776140&s=170667a&w=0&h=_GqFjqKp8_LP5_FFeMapbVFzMw7UMxEyiWNhLQYMasE="
+      />
+      <img
+        className="product-img"
+        src="https://homesourcetx-wpengine.netdna-ssl.com/wp-content/uploads/2021/02/Industrial-and-wooden-desk.jpg"
+      />
+      <img
+        className="product-img"
+        src="https://media.istockphoto.com/photos/yellow-artistic-lamp-with-rock-and-wooden-basement-on-the-table-picture-id1127365483?k=20&m=1127365483&s=170667a&w=0&h=mMKNACPJma1MCZPKJJsIZFzUMkVjxSxihn65Al-Zjq0="
+      />
+      <img
+        className="product-img"
+        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSQ6eg1NED1_i7sG-GRzpTxaht2SOIGRG7XSA&usqp=CAU"
+      />
+      <img className="product-img" src="https://www.kids-world.com/images/LO886.jpg" />
+    </div>
   </div>
 );
