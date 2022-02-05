296767f5348e51f73d6f87c744ee50ee05af1361
Elly <ellychau10@yahoo.com>
2022-02-05 15:17:50 -0800

    Style home products and add hover box-shadow effect on images

--- app/assets/javascripts/bundle.js
+++ app/assets/javascripts/bundle.js
@@ -336,22 +336,22 @@ exports.default = function () {
       "div",
       { className: "product-img-frame" },
       _react2.default.createElement("img", {
-        className: "product-img",
+        className: "product-img imagedropshadow",
         src: "https://media.istockphoto.com/photos/leather-minimalist-wallet-picture-id916776140?k=20&m=916776140&s=170667a&w=0&h=_GqFjqKp8_LP5_FFeMapbVFzMw7UMxEyiWNhLQYMasE="
       }),
       _react2.default.createElement("img", {
-        className: "product-img",
+        className: "product-img imagedropshadow",
         src: "https://homesourcetx-wpengine.netdna-ssl.com/wp-content/uploads/2021/02/Industrial-and-wooden-desk.jpg"
       }),
       _react2.default.createElement("img", {
-        className: "product-img",
+        className: "product-img imagedropshadow",
         src: "https://media.istockphoto.com/photos/yellow-artistic-lamp-with-rock-and-wooden-basement-on-the-table-picture-id1127365483?k=20&m=1127365483&s=170667a&w=0&h=mMKNACPJma1MCZPKJJsIZFzUMkVjxSxihn65Al-Zjq0="
       }),
       _react2.default.createElement("img", {
-        className: "product-img",
+        className: "product-img imagedropshadow",
         src: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSQ6eg1NED1_i7sG-GRzpTxaht2SOIGRG7XSA&usqp=CAU"
       }),
-      _react2.default.createElement("img", { className: "product-img", src: "https://www.kids-world.com/images/LO886.jpg" })
+      _react2.default.createElement("img", { className: "product-img imagedropshadow", src: "https://www.kids-world.com/images/LO886.jpg" })
     )
   );
 
