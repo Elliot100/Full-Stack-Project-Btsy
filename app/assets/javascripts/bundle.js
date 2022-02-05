505cbab6c0df2798b01322e36a58422e668ec066
Elly <ellychau10@yahoo.com>
2022-02-05 13:37:45 -0800

    Add search-bar and search-bar-button

--- app/assets/javascripts/bundle.js
+++ app/assets/javascripts/bundle.js
@@ -387,17 +387,42 @@ exports.default = function (_ref) {
     )
   );
 
-  var search_bar = _react2.default.createElement("input", { id: "global-enhancements-search-query",
-    "data-id": "search-query",
-    "data-search-input": "",
-    type: "text",
-    name: "search_query",
-    className: "search-bar",
-    placeholder: "Search for anything",
-    value: "",
-    autoComplete: "off",
-    autoCorrect: "off",
-    autoCapitalize: "off" });
+  var search_bar = _react2.default.createElement(
+    "div",
+    { className: "search-bar" },
+    _react2.default.createElement("input", {
+      id: "global-enhancements-search-query",
+      "data-id": "search-query",
+      "data-search-input": "",
+      type: "text",
+      name: "search_query",
+      className: "search-bar-input",
+      placeholder: "Search for anything",
+      value: "",
+      autoComplete: "off",
+      autoCorrect: "off",
+      autoCapitalize: "off"
+    }),
+    _react2.default.createElement(
+      "button",
+      {
+        type: "submit",
+        className: "search-bar-button",
+        value: "Search",
+        "aria-label": "Search",
+        "data-id": "gnav-search-submit-button" },
+      _react2.default.createElement(
+        "span",
+        { className: "search-bar-button-icon" },
+        _react2.default.createElement(
+          "svg",
+          { width: "24", height: "24", xmlns: "http://www.w3.org/2000/svg", viewBox: "0 0 24 24", "aria-hidden": "true", focusable: "false" },
+          _react2.default.createElement("path", { d: "M10,18a8,8,0,1,1,8-8A8.009,8.009,0,0,1,10,18ZM10,4a6,6,0,1,0,6,6A6.007,6.007,0,0,0,10,4Z" }),
+          _react2.default.createElement("path", { d: "M21,22a1,1,0,0,1-.707-0.293l-4-4a1,1,0,0,1,1.414-1.414l4,4A1,1,0,0,1,21,22Z" })
+        )
+      )
+    )
+  );
 
   var auth = currentUser ? _react2.default.createElement(
     "div",
--- frontend/components/nav_bar/nav_bar.jsx
+++ frontend/components/nav_bar/nav_bar.jsx
@@ -14,17 +14,34 @@ export default ({ currentUser, logout }) => {
   );
 
   const search_bar = (
-  <input id="global-enhancements-search-query" 
-    data-id="search-query" 
-    data-search-input="" 
-    type="text" 
-    name="search_query" 
-    className="search-bar" 
-    placeholder="Search for anything" 
-    value="" 
-    autoComplete="off" 
-    autoCorrect="off" 
-    autoCapitalize="off"/>
+    <div className="search-bar">
+      <input
+        id="global-enhancements-search-query"
+        data-id="search-query"
+        data-search-input=""
+        type="text"
+        name="search_query"
+        className="search-bar-input"
+        placeholder="Search for anything"
+        value=""
+        autoComplete="off"
+        autoCorrect="off"
+        autoCapitalize="off"
+      />
+      <button
+        type="submit"
+        className="search-bar-button"
+        value="Search"
+        aria-label="Search"
+        data-id="gnav-search-submit-button">
+        <span className="search-bar-button-icon">
+          <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" aria-hidden="true" focusable="false">
+            <path d="M10,18a8,8,0,1,1,8-8A8.009,8.009,0,0,1,10,18ZM10,4a6,6,0,1,0,6,6A6.007,6.007,0,0,0,10,4Z"></path>
+            <path d="M21,22a1,1,0,0,1-.707-0.293l-4-4a1,1,0,0,1,1.414-1.414l4,4A1,1,0,0,1,21,22Z"></path>
+          </svg>
+        </span>
+      </button>
+    </div>
   );
 
   const auth = currentUser ? (
